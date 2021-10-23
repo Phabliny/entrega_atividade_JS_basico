682fbd79476041374b45c57b213f5da5fe624079
Phabliny <phablinymartins@gmail.com>
2021-10-22 23:37:15 -0300

    Alteração na pag01

--- atividade_8/javascript.js
+++ atividade_8/javascript.js
@@ -4,6 +4,7 @@ function changeBoy(nameImg, msg) {
 
     if(nameImg == "alegre"){
         nome = prompt("Qual o seu nome?")
+        document.getElementById('imgBoy').src = `img/${nameImg}.png`
         document.getElementById("txtBoy").innerHTML = `${nome}, vamos brincar?`
     }
 }
